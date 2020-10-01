9e02a6ceb14f7a629626f0534d5cf0e48900bc92
Marcus Xavier <marcusxavierr123@gmail.com>
2020-10-01 11:08:51 -0300

    create onload

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-Todo: Create logic to game
+window.onload = function(){
+
+    String.prototype.repeat =  String.prototype.repeat ||
+    function(c){
+        var r= '';
+        for(var i=0; i<c; ++i);
+            r += this;
+        return r;
+    }
+    
+    
+    
+}
+    //window.onload = setTimeout(loadGame,2000);
+    
