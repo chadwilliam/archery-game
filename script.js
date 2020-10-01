2cec809e3c95108065573cf5fcf4aea6ff50da2b
Marcus Xavier <marcusxavierr123@gmail.com>
2020-10-01 11:09:46 -0300

    create startGame function

--- script.js
+++ script.js
@@ -7,9 +7,25 @@ window.onload = function(){
             r += this;
         return r;
     }
-    
-    
+    var startPage = document.getElementById("startMenu");
+    startPage.addEventListener("click",startGame)
+    function startGame(){
+        startPage.style.display = "none";
+        loadGame();
+        try{
+            startSound.play().catch(function(e){});
+            if(bgSound.paused) bgSound.play().catch(function(e){});
+            if(runCount == 0){
+            endSound.play().catch(function(e){})
+            hitSound.play().catch(function(e){});
+            successSound.play().catch(function(e){});
+            highScoreSound.play().catch(function(e){});
+            runCount++;
+            }
+        }catch(err){}
+        
+    }
     
 }
-    //window.onload = setTimeout(loadGame,2000);
+   
     
